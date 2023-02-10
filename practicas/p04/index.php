6faabcbd07395afea14a5120ec624ae509f060c1
Oscar01am <oscaramaro.01@gmail.com>
2023-02-09 23:50:55 -0600

    Ejercicio 5 terminado

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -93,5 +93,43 @@
             ?>
         </p>
     </div>
+    <hr>
+    <div>
+        <h3>Ejercicio 5</h3>
+        <p>
+            Usar las variables <strong>$edad</strong> y <strong>$sexo</strong> en una instrucción if para identificar una persona de sexo "femenino", 
+            cuya edad oscile entre los 18 y 35 años y mostrar un mensaje de bienvenida apropiado. Por ejemplo:
+        </p>
+        <p>
+            <em>Bienvenida, usted está en el rango de edad permitido.</em>
+        </p>
+        <p>
+            En caso contrario, deberá devolverse otro mensaje indicando el error.
+        </p>
+        <ul>
+            <li>Los valores para $edad y $sexo se deben obtener por medio de un formulario en HTML.</li>
+            <li>Utilizar el la Variable Superglobal $_POST (revisar documentación).</li>
+        </ul>
+        <p>
+            R:
+            <style>
+            fieldset {background-color: #adc2ce;}
+            legend {background-color: #118ab2 ;color: #f1faee;padding: 5px 10px;}
+            input {margin: 5px;}
+            </style>
+        </p>
+        <form id="form" action="./src/script1.php" method="post">
+        <fieldset>
+            <legend>Información Personal</legend>
+            <ol>
+            <li><label>Edad:</label> <input type="text" name="edad"></li>
+            <li><label>Sexo:</label> <input type="text" name="sexo"></li>
+            </ol>
+        </fieldset>
+        <p>
+            <input type="submit" value="¡OK!">
+        </p>
+        </form>
+    </div>
 </body>
 </html>
