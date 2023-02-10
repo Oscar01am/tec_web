67f3f8d044eb071ccf2a22c813633caa5619769d
Oscar01am <oscaramaro.01@gmail.com>
2023-02-09 23:47:11 -0600

    Descar de la plantilla de index.php descargada

---
+++ practicas/p04/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 3</title>
+    <style type="text/css">
+      ol { 
+      list-style-type: none;
+      }
+    </style>
+</head>
+<body>
+    <?php
+    require_once __DIR__.'/src/funciones.php';
+    ?>
+    <div>
+        <h3>Ejercicio 1</h3>
+        <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7.</p>
+        <p>
+            R:
+            <?php
+            if (!empty($_GET['numero'])) {
+                $numero = $_GET['numero'];
+                echo multiplo5y7($numero);
+            } else {
+                echo '(vacío)';
+            }
+            ?>
+        </p>
+    </div>
+ 
+
+
+ 
+    
+</body>
+</html>
