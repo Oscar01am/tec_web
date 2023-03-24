8d5cf677f749ef6a9228f756bb4836b7d67d2ac4
Oscar01am <oscaramaro.01@gmail.com>
2023-03-23 23:22:14 -0600

    Archivo read.php actualizado para la busqueda de proctudos

---
+++ practicas/p08/product_app/backend/read.php
@@ -0,0 +1,29 @@
+<?php
+    include_once __DIR__.'/database.php';
+
+    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    $data = array();
+    // SE VERIFICA HABER RECIBIDO EL ID
+    if( isset($_POST['search']) ) {
+        $search = $_POST['search'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        if ( $result = $conexion->query("SELECT * FROM productos where nombre LIKE '%{$search}%' or marca like '%{$search}%' or detalles like '%{$search}%'") ) {
+            // SE OBTIENEN LOS RESULTADOS
+			$row = $result->fetch_array(MYSQLI_ASSOC);
+
+            if(!is_null($row)) {
+                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                foreach($row as $key => $value) {
+                    $data[$key] = utf8_encode($value);
+                }
+            }
+			$result->free();
+		} else {
+            die('Query Error: '.mysqli_error($conexion));
+        }
+		$conexion->close();
+    } 
+    
+    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+    echo json_encode($data, JSON_PRETTY_PRINT);
+?>
